67eff26068ccd9cbf81557cde62f23c435214c8d
Manjula123-hi <danthulurimanjula@gmail.com>
2025-06-23 16:32:31 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "sg_name" {
+  default = string
+}
+
+variable "description" {
+  default = string
+}
+
+variable "project" {
+  default = string
+}
+
+variable "environment" {
+  default = string
+}
+
+variable "vpc_id" {
+  default = string
+}
