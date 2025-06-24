b59b0c92a418fe75d2b15cabc240c6fc38eedc3a
Manjula123-hi <danthulurimanjula@gmail.com>
2025-06-24 15:35:38 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "main" {
+  name        = var.sg_name
+  description = var.description
+  vpc_id      = var.vpc_id
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-${var.sg_name}"
+    }
+  )
+}
+
+# output "sg_id" {
+#   value = aws_security_group.main.id
+# }
